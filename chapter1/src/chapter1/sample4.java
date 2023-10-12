a30a34715f704217532b1f8f79b1e9dd3a41d3fb
junren0927 <junren0927@gmail.com>
2023-10-12 11:02:06 +0800

    first commit

---
+++ chapter1/src/chapter1/sample4.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class sample4 {
+	public static void main(String[] args)
+	{
+		System.out.println("顯示出反斜線:\\");
+		System.out.println("顯示出單引號\'");
+	}
+
+}
---
+++ chapter1/src/chapter1/sample6.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class sample6 {
+	public static void main(String[] args)
+	{
+		int num;
+		
+		num=3;
+		System.out.println("變數num的值是"+num);
+	}
+	
+
+}
